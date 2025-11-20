26cb8775c73bce71bba526fcc81d46e9cbaa02fd
Tanishk Sharma <vs1683183@gmail.com>
2025-11-20 20:15:21 +0530

    https://github.com/Tanishk168/FraudFree-Frontend.git

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:5000",
+    // baseURL:  "http://localhost:5000",
     timeout: 30000, // Increased from 3000ms to 30000ms (30 seconds) for AI requests
     headers: {
         'Content-Type': 'application/json',
