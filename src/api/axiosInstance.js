a70b378f72d31763ae5fb1f86a411d769f620e8c
Tanishk Sharma <vs1683183@gmail.com>
2025-11-20 20:15:21 +0530

    https://github.com/Tanishk168/FraudFree-Frontend.git

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.VITE_APP_API_URL || "http://localhost:5000",
-    // base url problem de rha tha env variable ki jagah normal locala ddress call ghora isliye abhi bnad rakha he isey
-    // baseURL: process.env.REACT_APP_API_URL,
+    baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:5000",
+    // baseURL:  "http://localhost:5000",
     timeout: 30000, // Increased from 3000ms to 30000ms (30 seconds) for AI requests
     headers: {
         'Content-Type': 'application/json',
