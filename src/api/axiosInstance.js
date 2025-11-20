9e33b451eb7d05c7035de234bc4ca6829b1a83c7
Tanishk Sharma <113034963+Tanishk168@users.noreply.github.com>
2025-11-20 19:46:36 +0530

    Refactor axiosInstance configuration
    Updated baseURL to use only the environment variable and increased timeout for requests.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+    // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+    // base url problem de rha tha env variable ki jagah normal locala ddress call ghora isliye abhi bnad rakha he isey
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 30000, // Increased from 3000ms to 30000ms (30 seconds) for AI requests
     headers: {
         'Content-Type': 'application/json',
